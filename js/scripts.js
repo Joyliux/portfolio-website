6c7d3645336c4b33ad478962eb24d9ff8613d1e7
Joyliux <joyli.uxdesign@gmail.com>
2023-01-12 16:08:20 -0500

    JS hamburger menu update

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
-alert("Hello, Weclome to My Portfolio Site");
-console.log("Hello world")
+/*  Access the hamburger and nav-menu classes.  */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+/*  Add a click event to the hamburger class to activate the menu. 
+If the menu is already displayed, it will simply hide it from view.  */
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+
+  /*  Also add a click event to each of the page links. 
+  Now when the user clicks on a link, the menu goes into hiding  */
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
+
+  

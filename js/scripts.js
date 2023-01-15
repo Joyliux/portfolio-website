c2107d9a01a6bc49d7c90f9334e0627fe473e875
Joyliux <joyli.uxdesign@gmail.com>
2023-01-15 14:19:02 -0500

    JS interacitivity

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,12 @@
-/*  Access the hamburger and nav-menu classes.  */
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-/*  Add a click event to the hamburger class to activate the menu. 
-If the menu is already displayed, it will simply hide it from view.  */
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  })
+$('#heading-text').text('How to create a hamburger menu with jQuery');
 
-  /*  Also add a click event to each of the page links. 
-  Now when the user clicks on a link, the menu goes into hiding  */
-  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  }))
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+	var isMobileBreakpoint = window.innerWidth <= 641
 
-  
+  if (x.className === 'navtoggle' && isMobileBreakpoint) {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
